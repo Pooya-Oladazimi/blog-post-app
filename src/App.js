7db5a1be631b3ecdf6c8547df9c3de72eb17c279
pooya.oa <pooya.oladazimi@tib.eu>
2023-06-01 13:44:55 +0200

    resize pane

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
 import {forLoopTypeOne, forLoopTypeTwo, forLoopTypeThree, forLoopTypeFour} from './components/ForLoops';
 import SortResults from './components/SortResults';
+import ResizeablePane from './components/paneResize';
+import './App.css';
 
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
           <Route exact path="/home/:tab?" component={Home} />
           <Route  path="/forloops" component={forLoopTypeOne} />
           <Route  path="/sort" component={SortResults} />
+          <Route  path="/resizePane" component={ResizeablePane} />
       </Switch>
       </BrowserRouter>
     </div>
---
+++ src/components/paneResize.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+
+
+
+class ResizeablePane extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = ({
+            lastPagePositionX: 0,
+            isResizeOn: false
+        });
+        this.onMouseDown = this.onMouseDown.bind(this);
+        this.moveToResize = this.moveToResize.bind(this);
+        this.releaseMouseFromResize = this.releaseMouseFromResize.bind(this);
+    }
+
+
+    onMouseDown(event){
+        let targetElement = event.target;
+        if (!targetElement.classList.contains('page-resize-vertical-line')){
+          return null;
+        }
+        this.lastPagePositionX = event.clientX;
+        this.setState({
+            isResizeOn: true
+        });
+    }
+    
+
+    moveToResize(event){
+        if(!this.state.isResizeOn){
+          return null;
+        }   
+        let addedWidth = (event.clientX - this.lastPagePositionX) / 1;    
+        let pageLeftPane = document.getElementById("page-left-pane");
+        let pageRightPane = document.getElementById("page-right-pane");
+        let currentWidthLeft = parseInt(pageLeftPane.offsetWidth);    
+        let currentWidthRight = parseInt(pageRightPane.offsetWidth);    
+        pageLeftPane.style.width = (currentWidthLeft + addedWidth) + "px";
+        pageRightPane.style.width = (currentWidthRight - addedWidth) + "px";
+        this.lastPagePositionX = event.clientX;        
+        this.setState({
+            lastPagePositionX: event.clientX
+        });
+      } 
+    
+
+    releaseMouseFromResize(event){
+        if(!this.state.isResizeOn){
+          return null;
+        }
+        this.setState({
+            isResizeOn: false
+        });    
+      }
+
+    
+    componentDidMount(){
+        document.body.addEventListener("mousedown", this.onMouseDown);
+        document.body.addEventListener("mousemove", this.moveToResize);
+        document.body.addEventListener("mouseup", this.releaseMouseFromResize);
+    }
+
+    componentWillUnmount(){
+        document.body.removeEventListener("mousedown", this.onMouseDown);
+        document.body.removeEventListener("mousemove", this.moveToResize);
+        document.body.removeEventListener("mouseup", this.releaseMouseFromResize);
+    }
+    
+    
+    render(){
+        return (
+            <div className="resizable-container">
+                <div className="pane-box" id="page-left-pane">
+                    Left Content
+                </div>
+                <div className='page-resize-vertical-line'></div>
+                <div className="pane-box" id="page-right-pane">
+                    Right Content
+                </div>
+            </div>
+        );
+    }
+
+
+}
+
+export default ResizeablePane;
